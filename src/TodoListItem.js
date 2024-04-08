a5a8a774699da22d481043fae4b85a6ae99d0c7a
Sathya Narayana Rangappagari <sathya.narayana@softsuave.org>
2024-04-08 13:18:17 +0530

    added class component

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,36 +1,81 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function TodoListItem() {
-  const [tasks, setTasks] = useState([]);
-  const [inputValue, setInputValue] = useState('');
+import React, { Component } from 'react';
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+// function TodoListItem() {
+//   const [tasks, setTasks] = useState([]);
+//   const [inputValue, setInputValue] = useState('');
+
+//   const handleInputChange = (e) => {
+//     setInputValue(e.target.value);
+//   };
+
+//   const handleAddTask = () => {
+//     if (inputValue.trim() !== '') {
+//       setTasks([...tasks, inputValue]);
+//       setInputValue('');
+//     }
+//   };
+
+//   return (
+//     <div class='center'>
+//       <input
+//         type="text"
+//         value={inputValue}
+//         onChange={handleInputChange}
+//         placeholder="Enter task"
+//       /> 
+//       <button onClick={handleAddTask}> Add Task </button>
+//       <ul>
+//         {tasks.map((task, index) => (
+//           <li key={index}>{task}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+class TodoListItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      inputValue: ''
+    };
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ inputValue: e.target.value });
   };
 
-  const handleAddTask = () => {
+  handleAddTask = () => {
+    const { tasks, inputValue } = this.state;
     if (inputValue.trim() !== '') {
-      setTasks([...tasks, inputValue]);
-      setInputValue('');
+      this.setState({
+        tasks: [...tasks, inputValue],
+        inputValue: ''
+      });
     }
   };
 
-  return (
-    <div class='center'>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Enter task"
-      /> 
-      <button onClick={handleAddTask}> Add Task </button>
-      <ul>
-        {tasks.map((task, index) => (
-          <li key={index}>{task}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  render() {
+    const { tasks, inputValue } = this.state;
+    return (
+      <div className='center'>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={this.handleInputChange}
+          placeholder="Enter task"
+        /> 
+        <button onClick={this.handleAddTask}> Add Task </button>
+        <ul class='center'>
+          {tasks.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default TodoListItem;
